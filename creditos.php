9100b1241345be207e482c7c255a51fc538ccfdf
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-19 10:33:03 -0600

    ADD:Creditos

---
+++ creditos.php
@@ -0,0 +1,5 @@
+ <h1>Creditos</h1>
+ </br>
+ <h2>Gabriela Ruiz Mendieta</h2>
+ </br>
+ <a href="index.php"> Mis Marple y trece problemas </a>
---
+++ index.php
@@ -0,0 +1,23 @@
+
+ <h1>Miss Marple y trece problemas </h1>
+</br>
+  <h2>de Agatha Christie</h2>
+</br> 
+  <p>Este libro contiene trece relatos cortos, en los que un grupo de personas        se reune todos los jueves para resolver misterios que la policia no ha
+      podido reolver, pero ninguno tiene la astucia de Miss Marple para               resolverlos </p>
+</br>
+
+<p>Funcion de acres a Metros</p>
+<?php
+  function AcresAMetros($ac){
+	  $mc= $ac/0.00024711;
+	  echo $mc;
+  }
+   
+  AcresAMetros(2);
+
+?>
+
+</br>
+<a href="creditos.php"> Creditos </a>
+
